00ca653c626cb1a39cf7595916d67b1f09a0a3cd
DanielBrv <daniel.bravop@pm.me>
2026-03-12 17:08:09 -0400

    Initial commit

---
+++ geojson_rasterizer/main.py
@@ -0,0 +1,40 @@
+import json
+import os.path
+import directory_dict
+# save states as serparate geojson files
+
+# select states
+# Reads in json containing list of states that have been selected by user
+def select_states() -> list:
+    print("reading states from selected_states.json")
+    try:
+        with open('../geojson/selected_states.json') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print("Error: The file 'selected_states.json' was not found.")
+    except json.JSONDecodeError:
+        print("Error: Failed to decode JSON from the file.")
+    return []       
+
+def open_geojson(states: list):
+    for state in states:
+        if state not in directory_dict.us_states_path:
+            print(f'Error: {state} is not a valid state')
+            continue
+
+        file_path = directory_dict.us_states_path[state]
+        if os.path.exists("../geojson/united_states/" + file_path):
+
+            print(f'{state} geojson file found')
+        else:
+            print(f'Error: {state} geojson file not found')
+
+open_geojson(select_states())
+# parse polygons
+
+# calc bounding box
+
+# ray casting
+
+# result
